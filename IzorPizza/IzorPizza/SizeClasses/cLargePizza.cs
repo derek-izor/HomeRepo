8cce6930847ba80597c0f6edbb8e42b5dbf2527a
Derek Izor <runawayvan@gmail.com>
2012-02-07 01:09:39 -0500

    Program is running, gui implemented, some minor bugs present

---
+++ IzorPizza/IzorPizza/SizeClasses/cLargePizza.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IzorPizza
+{
+    public class cLargePizza: cPizza
+    {
+        public cLargePizza()
+        {
+            description = "Large Pizza, ";
+            this.setSize("large");
+        }
+
+        public override double cost()
+        {
+            return 17.00;
+        }
+
+    }
+}
---
+++ IzorPizza/IzorPizza/SizeClasses/cMedPizza.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IzorPizza
+{
+    public class cMedPizza: cPizza
+    {
+        public cMedPizza()
+        {
+            description = "Medium Pizza, ";
+            this.setSize("medium");
+        }
+
+        public override double cost()
+        {
+            return 13.00;
+        }
+
+    }
+}
